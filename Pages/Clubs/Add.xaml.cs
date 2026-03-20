946d61e7d9fadffeb6ae06563039cc15c45f2167
robertshein <rober@DESKTOP-BDT2S8O>
2026-03-20 16:39:03 +0500

    азработанный модуль управления клубами. Шейн

---
+++ Pages/Clubs/Add.xaml.cs
@@ -0,0 +1,80 @@
+﻿using praktika26_Shein.Models;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace praktika26_Shein.Pages.Clubs
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        /// <summary>
+        /// Главная страница клубов
+        /// </summary>
+        private Main Main;
+
+        /// <summary>
+        /// Данные клуба
+        /// </summary>
+        private Models.Clubs Club;
+
+        public Add(Main Main, Models.Clubs Club = null)
+        {
+            InitializeComponent();
+
+            // Запоминаем в переменную
+            this.Main = Main;
+
+            // Если пришёл клуб, отображаем данные
+            if (Club != null)
+            {
+                // Запоминаем клуб в переменную
+                this.Club = Club;
+                // Указываем наименование
+                this.Name.Text = Club.Name;
+                // Указываем адрес
+                this.Address.Text = Club.Address;
+                // Указываем время работы
+                this.WorkTime.Text = Club.WorkTime;
+                // Изменяем текст кнопки
+                BtnAdd.Content = "Изменить";
+            }
+        }
+
+        /// <summary>
+        /// Метод добавления или изменения
+        /// </summary>
+        private void AddClub(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Если клуб пустой
+            if (this.Club == null)
+            {
+                // Создаём новый объект
+                Club = new Models.Clubs();
+                // Задаём данные
+                Club.Name = this.Name.Text;
+                Club.Address = this.Address.Text;
+                Club.WorkTime = this.WorkTime.Text;
+                // Добавляем объект в контекст
+                this.Main.AllClub.Clubs.Add(this.Club);
+                // Сохраняем изменения
+                this.Main.AllClub.SaveChanges();
+            }
+            else
+            {
+                // Если изменение
+                // Изменяем данные
+                Club.Name = this.Name.Text;
+                Club.Address = this.Address.Text;
+                Club.WorkTime = this.WorkTime.Text;
+                // Сохраняем изменения
+                this.Main.AllClub.SaveChanges();
+            }
+
+            // Открываем страницу клубов
+            MainWindow.init.OpenPages(new Pages.Clubs.Main());
+        }
+    }
+}
---
+++ Pages/Clubs/Elements/Item.xaml.cs
@@ -0,0 +1,69 @@
+﻿using praktika26_Shein.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace praktika26_Shein.Pages.Clubs.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Item.xaml
+    /// </summary>
+    public partial class Item : UserControl
+    {
+        /// <summary>
+        /// Главная страница клубов
+        /// </summary>
+        private Main Main;
+
+        /// <summary>
+        /// Данные клуба
+        /// </summary>
+        private Models.Clubs Club;
+
+        public Item(Models.Clubs Club, Main Main)
+        {
+            InitializeComponent();
+
+            // Присваиваем наименование
+            this.Name.Text = Club.Name;
+            // Присваиваем адрес
+            this.Address.Text = Club.Address;
+            // Присваиваем время работы
+            this.WorkTime.Text = Club.WorkTime;
+
+            // Запоминаем в переменные
+            this.Main = Main;
+            this.Club = Club;
+        }
+
+        private void EditClub(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Открываем страницу добавления, пересылая данные
+            MainWindow.init.OpenPages(new Pages.Clubs.Add(this.Main, this.Club));
+        }
+
+        /// <summary>
+        /// Метод удаления
+        /// </summary>
+        private void DeleteClub(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Удаляем клуб из контекста
+            Main.AllClub.Clubs.Remove(Club);
+            // Сохраняем изменения
+            Main.AllClub.SaveChanges();
+            // Удаляем элемент со страницы Main
+            (Main.Parent as Panel)?.Children.Remove(this);
+        }
+    }
+}
